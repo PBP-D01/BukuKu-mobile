9ba1aa26e41fbe91be1902cba4b8df3b22dc6c55
Nabila Zavira <nabilazavira@ui.ac.id>
2023-11-27 14:11:46 +0700

    login, register

---
+++ lib/screens/menu.dart
@@ -0,0 +1,70 @@
+import 'package:bukuku/widgets/card.dart';
+import 'package:bukuku/widgets/left_drawer.dart';
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+    final int id;
+    MyHomePage({Key? key, required this.id}) : super(key: key);
+    final List<ShopItem> items = [
+    ShopItem("Lihat Item", Icons.checklist, const Color.fromARGB(255, 195, 146, 224)),
+    ShopItem("Tambah Item", Icons.add_shopping_cart, const Color.fromARGB(255, 135, 97, 157)),
+    ShopItem("Logout", Icons.logout,Color.fromARGB(255, 101, 70, 118)),
+];
+    
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Pacil Library',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor:  const Color.fromARGB(255, 110, 176, 93),
+        foregroundColor: Colors.white,
+      ),
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      drawer: LeftDrawer(id: id,),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'Pacil Library', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card kita.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((ShopItem item) {
+                  // Iterasi untuk setiap item
+                  return ShopCard(item, id);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  
+}
+ 
---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:bukuku/screens/menu.dart';
+
+
+class LeftDrawer extends StatelessWidget {
+  final int id;
+  const LeftDrawer({super.key, required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            // TODO: Bagian drawer header
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 175, 128, 196),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Pacil Library',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    Text(
+                    "Catat seluruh keperluan belanjamu di sini!",
+                    style: TextStyle(
+                      fontSize: 15,      // Ukuran font 15
+                      color: Colors.white,  // Warna teks putih
+                      fontWeight: FontWeight.normal,  // Weight biasa
+                    ),
+                    textAlign: TextAlign.center, // Center alignment
+                  )
+                  ],
+                ),
+              ),
+          
+          // TODO: Bagian routing
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Halaman Utama'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(id:id),
+                  ));
+            },
+          ),
+        
+        ],
+      ),
+    );
+  }
+}
